c80f583a0776b4bd7e75953a0b0ae7cce713c41b
Sebastian Jug <seb@stianj.ug>
2020-10-14 13:09:00 -0400

    Deployment succesfully working on RHCOS node.   - Updated README.md for podman deployment   - Updated start.sh entrypoint script

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 This container is created on a base UBI8 image. Then we install wireguard inside the container so we can use it on an RHCOS node.
 
+## OpenShift
+
 A sample `pod.yml` is included for OpenShift (k8s) deployment.
+
+## RHCOS
+
+To deploy the `wg-rhcos` container on a bare RHCOS node:
+
+```
+sudo podman run -d \
+  --privileged \
+  --network host \
+  --mount type=bind,source=/etc/resolv.conf,target=/etc/resolv.conf \
+  -v /etc/os-release:/etc/os-release:ro \
+  -v /var/lib/containers/storage/overlay:/tmp/overlay:ro \
+  -v /etc/wireguard:/etc/wireguard \
+  --restart on-failure \
+  quay.io/jugs/wg-rhcos
+```
--- start.sh
+++ start.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -ex
 
+RHOCP_VERSION=$(awk -F'"' '/VERSION_ID/{print $2}' /etc/os-release)
+SERVER_IP=${SERVER_IP:-192.168.111.1}
+DNS_IP=${DNS_IP:-192.168.222.1}
 KERNEL_VERSION=$(uname -r)
 KERNEL_CORE=$(find /tmp/overlay -name kernel-core-${KERNEL_VERSION}.rpm -exec ls {} \; | tail -n1)
 KERNEL_DEVEL=$(find /tmp/overlay -name kernel-devel-${KERNEL_VERSION}.rpm -exec ls {} \; | tail -n1)
 KERNEL_HEADERS=$(find /tmp/overlay -name kernel-headers-${KERNEL_VERSION}.rpm -exec ls {} \; | tail -n1)
+
+catch() {
+  cp /etc/resolv.conf.bak /etc/resolv.conf
+  wg-quick down wg0
+}
+
+set_dns() {
+  cp /etc/resolv.conf /etc/resolv.conf.bak
+  printf 'nameserver %s\n' "${DNS_IP}" > /etc/resolv.conf
+}
 
 if [ -z "${KERNEL_CORE}" ] || [ -z "${KERNEL_DEVEL}" ] || [ -z "${KERNEL_HEADERS}" ]; then
   KERNEL_CORE=kernel-core-${KERNEL_VERSION}
@@ -12,9 +25,15 @@ if [ -z "${KERNEL_CORE}" ] || [ -z "${KERNEL_DEVEL}" ] || [ -z "${KERNEL_HEADERS
   KERNEL_HEADERS=kernel-headers-${KERNEL_VERSION}
 fi
 
-dnf install -y --enablerepo=rhocp-4.3-for-rhel-8-x86_64-rpms $KERNEL_DEVEL $KERNEL_HEADERS $KERNEL_CORE
+dnf install -y --enablerepo=rhocp-${RHOCP_VERSION}-for-rhel-8-x86_64-rpms ${KERNEL_DEVEL} ${KERNEL_HEADERS} ${KERNEL_CORE}
+dnf install -y iproute iputils kmod-wireguard wireguard-tools
+dnf clean packages
 
-dnf install -y kmod-wireguard wireguard-tools
-
-#systemctl enable --now wg-quick@wg0.service
+modprobe wireguard
+trap catch err exit
 wg-quick up wg0
+set_dns
+ping -c 3 ${SERVER_IP}
+wg
+
+sleep 86400
